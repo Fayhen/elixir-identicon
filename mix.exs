c88cf3847db02ade9639e6ce99f3d698b5f76dbb
Diego Garcia Cordeiro Souza <diego00alfa@gmail.com>
2023-02-28 20:38:10 -0300

    feat: new version boilerplate
    Re-initialized the repository with boilerplate code and an updated
    Elixir version.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ElixirIdenticon.MixProject do
     [
       app: :elixir_identicon,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -14,14 +14,15 @@ defmodule ElixirIdenticon.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :crypto]
+      extra_applications: [:logger]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:egd, github: "erlang/egd"}
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
