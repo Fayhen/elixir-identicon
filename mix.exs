47022efde69e42049bda9dd3960f71461fed0d66
Diego Garcia Cordeiro Souza <diego00alfa@gmail.com>
2023-02-28 23:57:49 -0300

    chore: bump version to 0.1.1
    Bumped application version from 0.1.0 to 0.1.1.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirIdenticon.MixProject do
   def project do
     [
       app: :elixir_identicon,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
